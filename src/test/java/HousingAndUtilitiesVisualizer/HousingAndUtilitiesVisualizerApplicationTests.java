6c36f1e579f0cbe974655e1e9fad505829770df9
dan <danielpechurin@gmail.com>
2022-11-08 17:53:22 +0500

    Initial commit

---
+++ src/test/java/HousingAndUtilitiesVisualizer/HousingAndUtilitiesVisualizerApplicationTests.java
@@ -0,0 +1,13 @@
+package HousingAndUtilitiesVisualizer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HousingAndUtilitiesVisualizerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
