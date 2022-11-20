414ae0bc6d6b12da4b823d9e50a68a4da70ee8fe
dan <danielpechurin@gmail.com>
2022-11-20 14:51:03 +0500

    add Null metrics check in MetricsService

--- src/main/java/HousingAndUtilitiesVisualizer/service/MetricsService.java
+++ src/main/java/HousingAndUtilitiesVisualizer/service/MetricsService.java
@@ -1,9 +1,15 @@
 package HousingAndUtilitiesVisualizer.service;
 
 import HousingAndUtilitiesVisualizer.builder.MetricsBuilder;
+import HousingAndUtilitiesVisualizer.model.ColdWaterMetrics;
+import HousingAndUtilitiesVisualizer.model.ElectricPowerMetrics;
+import HousingAndUtilitiesVisualizer.model.HeatingMetrics;
+import HousingAndUtilitiesVisualizer.model.HotWaterMetrics;
 import HousingAndUtilitiesVisualizer.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
 
 @Service
 public class MetricsService {
@@ -19,9 +25,16 @@ public class MetricsService {
 
 
     public void save(MetricsBuilder metricsBuilder) {
-        coldWaterRepository.save(metricsBuilder.getColdWaterMetrics());
-        hotWaterRepository.save(metricsBuilder.getHotWaterMetrics());
-        heatingRepository.save(metricsBuilder.getHeatingMetrics());
-        electricPowerRepository.save(metricsBuilder.getElectricPowerMetrics());
+
+
+        ColdWaterMetrics coldWaterMetrics = metricsBuilder.getColdWaterMetrics();
+        HotWaterMetrics hotWaterMetrics = metricsBuilder.getHotWaterMetrics();
+        HeatingMetrics heatingMetrics = metricsBuilder.getHeatingMetrics();
+        ElectricPowerMetrics electricPowerMetrics = metricsBuilder.getElectricPowerMetrics();
+
+        if (coldWaterMetrics != null) coldWaterRepository.save(coldWaterMetrics);
+        if (hotWaterMetrics != null) hotWaterRepository.save(hotWaterMetrics);
+        if (heatingMetrics != null) heatingRepository.save(heatingMetrics);
+        if (electricPowerMetrics != null) electricPowerRepository.save(electricPowerMetrics);
     }
 }
