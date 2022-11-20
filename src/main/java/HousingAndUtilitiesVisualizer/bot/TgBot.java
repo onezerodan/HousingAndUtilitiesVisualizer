0612a452c7456404e320d244287ca956b71dedab
dan <danielpechurin@gmail.com>
2022-11-20 15:53:22 +0500

    add basic TgBot

--- src/main/java/HousingAndUtilitiesVisualizer/bot/TgBot.java
+++ src/main/java/HousingAndUtilitiesVisualizer/bot/TgBot.java
@@ -1,21 +1,43 @@
 package HousingAndUtilitiesVisualizer.bot;
 
-import org.telegram.telegrambots.bots.TelegramLongPollingBot;
-import org.telegram.telegrambots.meta.api.objects.Update;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+
+@Component
 public class TgBot extends TelegramLongPollingBot {
+
+    @Autowired
+    BotConfig botConfig;
+
+    Logger log = LogManager.getLogger(TgBot.class);
     @Override
     public String getBotUsername() {
-        return null;
+        return botConfig.getUsername();
     }
 
     @Override
     public String getBotToken() {
-        return null;
+        return botConfig.getToken();
     }
 
     @Override
     public void onUpdateReceived(Update update) {
-
+        log.info(update.getMessage().getText());
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.setChatId(update.getMessage().getChatId());
+        sendMessage.setText(update.getMessage().getText());
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
