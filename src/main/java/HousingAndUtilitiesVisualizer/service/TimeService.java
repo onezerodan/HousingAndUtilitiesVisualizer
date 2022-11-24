b90bd7a328521b311cf7722e8add4e2657458a33
dan <danielpechurin@gmail.com>
2022-11-24 18:02:29 +0500

    add TimeService

---
+++ src/main/java/HousingAndUtilitiesVisualizer/service/TimeService.java
@@ -0,0 +1,34 @@
+package HousingAndUtilitiesVisualizer.service;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
+
+@Service
+public class TimeService {
+
+    Logger log = LogManager.getLogger(TimeService.class);
+
+    public Date getCurrentDate() {
+        SimpleDateFormat formatter = new SimpleDateFormat(
+                "dd/MM/yyyy");
+        Date date = null;
+        try {
+            date = formatter.parse(formatter.format(new Date()));
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return date;
+    }
+
+    public Date parseDateFromStr(String dateStr) throws ParseException {
+        SimpleDateFormat formatter = new SimpleDateFormat("dd-MM-yyyy");
+        Date date = formatter.parse(dateStr);
+        return date;
+    }
+}
