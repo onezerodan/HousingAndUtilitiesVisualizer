94187109ee90acc6df8869300c0da1c34948a564
dan <danielpechurin@gmail.com>
2022-12-02 23:11:45 +0500

    ChartService

---
+++ src/main/java/HousingAndUtilitiesVisualizer/service/ChartService.java
@@ -0,0 +1,96 @@
+package HousingAndUtilitiesVisualizer.service;
+
+
+import HousingAndUtilitiesVisualizer.model.*;
+import HousingAndUtilitiesVisualizer.repository.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jfree.chart.ChartColor;
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartUtilities;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+import org.jfree.data.general.Dataset;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+import java.util.List;
+
+import static java.util.Comparator.comparing;
+
+@Service
+public class ChartService {
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    MetricsService metricsService;
+    @Autowired
+    TimeService timeService;
+
+    Logger log = LogManager.getLogger(ChartService.class);
+
+    public File getChart(Long userId, Period period) {
+        User user = userRepository.findByChatId(userId);
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+        for (Map.Entry<MetricsType, List<?>> entry
+                : metricsService.getMetricsForPeriod(period, user).entrySet()) {
+            MetricsType metricsType = entry.getKey();
+            List<Metrics> metricsList = (List<Metrics>) entry.getValue();
+
+            metricsList.sort(comparing(Metrics::getDateAdded));
+            for (Metrics metrics : metricsList) {
+                if (metricsType == MetricsType.ELECTRIC_POWER) {
+                    ElectricPowerMetrics electricMetrics = (ElectricPowerMetrics) metrics;
+
+                    dataset.addValue(electricMetrics.getValueDay(),
+                            "Электроэнергия день",
+                            timeService.dateToStr(electricMetrics.getDateAdded()));
+                    dataset.addValue(electricMetrics.getValueNight(),
+                            "Электроэнергия ночь",
+                            timeService.dateToStr(electricMetrics.getDateAdded()));
+                    continue;
+
+                }
+                dataset.addValue(metrics.getValue(),
+                        metricsType.label,
+                        timeService.dateToStr(metrics.getDateAdded()));
+            }
+
+        }
+
+        File result = null;
+        try {
+            result = drawChart(dataset, userId);
+        } catch (Exception e) {
+            log.error(e.getMessage(), e);
+        }
+        return result;
+    }
+
+    private File drawChart(DefaultCategoryDataset dataset, Long userId) throws IOException {
+        JFreeChart lineChart = ChartFactory.createLineChart(
+                "Потребление ЖКУ", // Chart title
+                "Дата", // X-Axis Label
+                "Показание", // Y-Axis Label
+                dataset,
+                PlotOrientation.VERTICAL,
+                true,
+                true,
+                false
+        );
+
+        lineChart.setAntiAlias(false);
+        File file = new File("data/charts/stat-" + userId +".png");
+        ChartUtilities.saveChartAsPNG(file, lineChart, 900, 600);
+        return file;
+    }
+
+
+}
---
+++ src/test/java/HousingAndUtilitiesVisualizer/ChartServiceTest.java
@@ -0,0 +1,16 @@
+package HousingAndUtilitiesVisualizer;
+
+import HousingAndUtilitiesVisualizer.service.ChartService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.io.IOException;
+
+@SpringBootTest
+public class ChartServiceTest {
+    @Autowired
+    ChartService chartService;
+
+
+}
