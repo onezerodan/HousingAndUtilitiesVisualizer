ee19d90ea938bd41f9d1483fb0f8d60d82d9cf35
dan <danielpechurin@gmail.com>
2022-12-02 23:12:31 +0500

    Metrics superclass update

--- src/main/java/HousingAndUtilitiesVisualizer/model/Metrics.java
+++ src/main/java/HousingAndUtilitiesVisualizer/model/Metrics.java
@@ -1,4 +1,50 @@
 package HousingAndUtilitiesVisualizer.model;
 
+import java.util.Date;
+
 public class Metrics {
+
+    private Long id;
+
+
+    private Date dateAdded;
+
+    private Double value;
+
+
+
+    private User user;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getDateAdded() {
+        return dateAdded;
+    }
+
+    public void setDateAdded(Date dateAdded) {
+        this.dateAdded = dateAdded;
+    }
+
+    public Double getValue() {
+        return value;
+    }
+
+    public void setValue(Double value) {
+        this.value = value;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
